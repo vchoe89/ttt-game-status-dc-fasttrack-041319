dc179deb9b5e5c29281303f3b9300fadde8cad0c
Victor Jian Choe <vchoe89@gmail.com>
2019-04-14 18:44:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,23 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
+  
+  
+  def won(board)
+  index.each WIN_COMBINATIONS = 1
+  
+  
+  
+  
